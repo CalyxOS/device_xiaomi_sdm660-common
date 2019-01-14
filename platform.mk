1a01dfe211cec02a4f46fb18866c48e5792cd17d
Chirayu Desai <chirayudesai1@gmail.com>
2019-01-14 19:51:24 +0530

    drop telephony-ext

--- platform.mk
+++ platform.mk
@@ -377,13 +377,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	$(PLATFORM_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-# Telephony
-PRODUCT_PACKAGES += \
-	telephony-ext
-
-PRODUCT_BOOT_JARS += \
-	telephony-ext
-
 # TextClassifier
 PRODUCT_PACKAGES += \
 	textclassifier.bundle1
