a3f4d977a34f67b8c446c3c51eea273aa4491146
Chirayu Desai <chirayudesai1@gmail.com>
2019-07-13 00:47:28 +0530

    sdm660-common: Build messaging app
    Change-Id: Ia539ac340348a29bbdd2f33f7e8e1b92e0fac52a

--- sdm660.mk
+++ sdm660.mk
@@ -306,6 +306,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Messaging
+PRODUCT_PACKAGES += \
+    messaging
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
